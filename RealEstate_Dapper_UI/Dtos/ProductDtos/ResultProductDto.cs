8eacd33483d247c67201644f6522d8ba024f431c
Mehmet Sünbül <mehmetsunbullktu@gmail.com>
2024-03-01 16:11:44 +0300

    .

--- RealEstate_Dapper_UI/Dtos/ProductDtos/ResultProductDto.cs
+++ RealEstate_Dapper_UI/Dtos/ProductDtos/ResultProductDto.cs
@@ -2,11 +2,15 @@
 {
     public class ResultProductDto
     {
-            public int productID { get; set; }
-            public string title { get; set; }
-            public decimal price { get; set; }
-            public string city { get; set; }
-            public string district { get; set; }
-            public int productCategory { get; set; }
+        public int productID { get; set; }
+        public string title { get; set; }
+        public decimal price { get; set; }
+        public string city { get; set; }
+        public string district { get; set; }
+        public string categoryName { get; set; }
+        public string coverimage { get; set; }
+        public string type { get; set; }
+        public string address { get; set; }
+        public bool dealOfTheDay { get; set; }
     }
 }
